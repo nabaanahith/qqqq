0c9d15865ed550987e6093de0a42906a4d21bc85
unknown <nabaa.nahith@qi.iq>
2020-05-23 23:53:02 +0300

    vv

---
+++ build/precache-manifest.c92eb8071595e28aa4438fd161a24c0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d70d520972df17fc531d52f0dfbffe6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "467689e23c97072a2422",
+    "url": "/static/css/main.fb5e79b7.chunk.css"
+  },
+  {
+    "revision": "14cb929b08e410307d2b",
+    "url": "/static/js/2.6f4d5df1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6f4d5df1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "467689e23c97072a2422",
+    "url": "/static/js/main.d13fc0f3.chunk.js"
+  },
+  {
+    "revision": "c9d005d02a0310f70245",
+    "url": "/static/js/runtime-main.b334ea73.js"
+  }
+]);
